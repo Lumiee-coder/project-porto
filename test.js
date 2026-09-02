2aaaefaf88bfb8807a2f38812230246183805aca
Lumiee-coder <azkaelcharida.rembang@gmail.com>
2026-09-02 09:00:43 +0700

    fix the bug

---
+++ test.js
@@ -0,0 +1,12 @@
+const myName = "azka";
+const myMidname = "EL";
+const myLastname = "Charida";
+const className = "RPL B";
+
+
+
+console.log(myName);
+console.log(myMidname);
+console.log(myLastname);
+console.log(className);
+console.log(`Halo pak jokowi, saya ${myName} ${myMidname} ${myLastname} dari kelas ${className}`);
